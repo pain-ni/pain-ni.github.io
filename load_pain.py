6502ea5c4b659a5556ccca3cbc8848f09f173ef4
Phil Weir <phil.weir@flaxandteal.co.uk>
2017-06-03 18:16:30 +0100

    update mock pain and user data

--- load_pain.py
+++ load_pain.py
@@ -1,22 +1,41 @@
 import json
 import random
 
+areas = [
+    "Back",
+    "Sensory",
+    "Arms",
+    "Leg",
+    "Neck",
+    "Shoulder",
+    "All"
+]
+
+types = [
+    "Burning",
+    "Pulsing",
+    "Throbbing",
+    "Numb"
+]
+
 with open('user_points.geojson', 'r') as f:
-    user_points = json.load(f)['features']
+    user_points = json.load(f)['features'][::100]
     entries = []
     points_per_user = len(user_points) // 5
+    type_id = random.randint(0, len(types) - 1)
+    area_id = random.randint(0, len(areas) - 1)
     for n, user_point in enumerate(user_points):
         entries.append({
             "id": n,
             "user_id": n // points_per_user,
             "category": "ENTRY_PAIN",
             "type": {
-              "id": 2,
-              "name": "burning",
+              "id": type_id,
+              "name": types[type_id],
               "parent_id": "1",
               "parent_name": "physical"
             },
-            "area": "Back",
+            "area": areas[area_id],
             "score": 0.1 * random.randrange(0, 100),
             "log_time": "2017-04-%02d %02d:%02d:%02d" % (
                 random.randint(0, 29),
