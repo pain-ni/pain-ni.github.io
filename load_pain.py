5b011219b21b16e8b90735523eb1124ecdeb02fd
Phil Weir <phil.weir@flaxandteal.co.uk>
2017-06-03 18:31:14 +0100

    vary pain data more

--- load_pain.py
+++ load_pain.py
@@ -11,31 +11,31 @@ areas = [
     "All"
 ]
 
-types = [
+types = list(enumerate([
     "Burning",
     "Pulsing",
     "Throbbing",
     "Numb"
-]
+]))
 
 with open('user_points.geojson', 'r') as f:
     user_points = json.load(f)['features'][::100]
     entries = []
     points_per_user = len(user_points) // 5
-    type_id = random.randint(0, len(types) - 1)
-    area_id = random.randint(0, len(areas) - 1)
     for n, user_point in enumerate(user_points):
+        type_id, type_name = random.choice(types)
+        area = random.choice(areas)
         entries.append({
             "id": n,
             "user_id": n // points_per_user,
             "category": "ENTRY_PAIN",
             "type": {
               "id": type_id,
-              "name": types[type_id],
+              "name": type_name,
               "parent_id": "1",
               "parent_name": "physical"
             },
-            "area": areas[area_id],
+            "area": area,
             "score": 0.1 * random.randrange(0, 100),
             "log_time": "2017-04-%02d %02d:%02d:%02d" % (
                 random.randint(0, 29),
